c93804b68fcb9cb3a29edd7382e2a258cdb7e95d
njdup <njdupp@gmail.com>
2015-02-21 11:49:43 -0800

    Added phone number setting as a secret field

--- config/secrets.js
+++ config/secrets.js
@@ -3,7 +3,8 @@
 module.exports = {
   twilio: {
     twilio_account_id: process.env.TWILIO_ID || 'bogus',
-    twilio_auth_token: process.env.TWILIO_AUTH || 'bogus'
+    twilio_auth_token: process.env.TWILIO_AUTH || 'bogus',
+    twilio_phone_number: process.env.TWILIO_NUMBER || '8888881994'
   }
 
 }
