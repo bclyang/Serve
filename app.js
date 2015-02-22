a36898317d5edd1506a5145436aa05d23f3bcaf7
Vic <vic317_yeh@hotmail.com>
2015-02-22 09:12:39 -0800

    added sendgrid

--- app.js
+++ app.js
@@ -11,6 +11,8 @@ var flash = require('connect-flash');
 var session = require('express-session');
 var mongoose = require('mongoose');
 var settings = require('./config/settings');
+var sendgrid  = require('sendgrid')('treehackssms', 'Treehacksrocks15');
+
 
 require('./config/passport')(passport); // pass passport for configuration
 
